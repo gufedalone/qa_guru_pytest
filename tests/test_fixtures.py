b24fbaeee2e5baa243b769f08efcf3c6ee96aabf
vlad <khalvlalove@gmail.com>
2022-08-10 18:29:21 +0400

    add xfail to tests/test_fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,24 +2,30 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='function')
-def stretched():
-    browser.config.window_width = 1600
-    browser.config.window_height = 900
+@pytest.fixture(params=[(1920, 1080), (1600, 900)], scope='function')
+def stretched(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    browser.open('https://github.com/')
 
 
-@pytest.fixture(scope='function')
-def shrinked():
-    browser.config.window_width = 828
-    browser.config.window_height = 1792
+@pytest.fixture(params=[(750, 1334), (1920, 1080)], scope='function')
+def shrinked(request):
+    browser.config.window_width = request.param[0]
+    browser.config.window_height = request.param[1]
+    browser.open('https://github.com/')
 
 
 def test_github_desktop(stretched):
-    browser.open('https://github.com/')
-    browser.element('a[href="/login"]').click()
+    try:
+        browser.element('a[href="/login"]').click()
+    except AssertionError:
+        pytest.xfail(reason="Mobile resolution")
 
 
 def test_github_mobile(shrinked):
-    browser.open('https://github.com/')
-    browser.element('svg.octicon-three-bars').click()
-    browser.element('a[href="/login"]').click()
+    try:
+        browser.element('svg.octicon-three-bars').click()
+        browser.element('a[href="/login"]').click()
+    except AssertionError:
+        pytest.xfail(reason="Desktop resolution")
