7de314ec9d74c5e21728f23bcc6a803ac049464e
vlad <khalvlalove@gmail.com>
2022-08-10 16:58:11 +0400

    add xfail

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -2,20 +2,25 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def browser_window_size(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
+    browser.open('https://github.com/')
 
 
-@pytest.mark.parametrize("browser_window_size", [(1600, 900)], indirect=True)
+@pytest.mark.parametrize("browser_window_size", [(1600, 900), (828, 1792)], indirect=True)
 def test_github_desktop():
-    browser.open('https://github.com/')
-    browser.element('a[href="/login"]').click()
+    try:
+        browser.element('a[href="/login"]').click()
+    except AssertionError:
+        pytest.xfail(reason="Mobile resolution")
 
 
-@pytest.mark.parametrize("browser_window_size", [(750, 1334)], indirect=True)
+@pytest.mark.parametrize("browser_window_size", [(750, 1334), (1920, 1080)], indirect=True)
 def test_github_mobile():
-    browser.open('https://github.com/')
-    browser.element('svg.octicon-three-bars').click()
-    browser.element('a[href="/login"]').click()
+    try:
+        browser.element('svg.octicon-three-bars').click()
+        browser.element('a[href="/login"]').click()
+    except AssertionError:
+        pytest.xfail(reason="Desktop resolution")
