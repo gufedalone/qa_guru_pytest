e21b11f89f2a8a7f9f7da3af5cc405aa7e3df1ba
vlad <khalvlalove@gmail.com>
2022-08-10 18:30:04 +0400

    edit fixture to tests/test_skip

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,23 +1,25 @@
+import time
 import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(params=[(1920, 1080), (1600, 900), (828, 1792), (750, 1334)], scope='session', autouse=True)
+@pytest.fixture(params=[(1920, 1080), (1600, 900), (828, 1792), (750, 1334)],
+                ids=["1920x1080", "1600x900", "828x1792", "750x1334"], scope='function', autouse=True)
 def browser_window_size(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
+    time.sleep(1)
+    browser.open('https://github.com/')
 
 
 def test_github_desktop(browser_window_size):
-    browser.open('https://github.com/')
-    if browser.config.window_width < 1012:
+    if (browser.config.window_width / browser.config.window_height) < 1:
         pytest.skip('Mobile resolution')
     browser.element('a[href="/login"]').click()
 
 
 def test_github_mobile(browser_window_size):
-    browser.open('https://github.com/')
-    if browser.config.window_width > 1011:
+    if (browser.config.window_width / browser.config.window_height) > 1:
         pytest.skip('Desktop resolution')
     browser.element('svg.octicon-three-bars').click()
     browser.element('a[href="/login"]').click()
