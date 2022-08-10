c7e8387e41a29355e0d48e334284f27174c36120
vlad <khalvlalove@gmail.com>
2022-08-10 18:51:06 +0400

    add ids to pytest.mark.parametrize

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -1,15 +1,24 @@
+import time
 import pytest
 from selene.support.shared import browser
+
+
+resolution = pytest.mark.parametrize("browser_window_size",
+                                     [(1920, 1080), (1600, 900), (828, 1792), (750, 1334)],
+                                     ids=["1920x1080", "1600x900", "828x1792", "750x1334"],
+                                     indirect=True
+                                     )
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_window_size(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
+    time.sleep(1)
     browser.open('https://github.com/')
 
 
-@pytest.mark.parametrize("browser_window_size", [(1600, 900), (828, 1792)], indirect=True)
+@resolution
 def test_github_desktop():
     try:
         browser.element('a[href="/login"]').click()
@@ -17,7 +26,7 @@ def test_github_desktop():
         pytest.xfail(reason="Mobile resolution")
 
 
-@pytest.mark.parametrize("browser_window_size", [(750, 1334), (1920, 1080)], indirect=True)
+@resolution
 def test_github_mobile():
     try:
         browser.element('svg.octicon-three-bars').click()
