efcc4a67e5890483855d7cf18b8941ec8b388720
vlad <khalvlalove@gmail.com>
2022-08-10 18:49:51 +0400

    add ids to fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,14 +2,14 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(params=[(1920, 1080), (1600, 900)], scope='function')
+@pytest.fixture(params=[(1920, 1080), (1600, 900)], ids=["1920x1080", "1600x900"], scope='function')
 def stretched(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
     browser.open('https://github.com/')
 
 
-@pytest.fixture(params=[(750, 1334), (1920, 1080)], scope='function')
+@pytest.fixture(params=[(750, 1334), (828, 1792)], ids=["750x1334", "828x1792"], scope='function')
 def shrinked(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
