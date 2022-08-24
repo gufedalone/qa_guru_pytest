26dc902ecae219482594242bb11cdcc6ce131243
vlad <khalvlalove@gmail.com>
2022-08-24 16:46:17 +0400

    parametrized with indirect

--- tests/test_parametrize.py
+++ tests/test_parametrize.py
@@ -2,15 +2,22 @@ import time
 import pytest
 from selene.support.shared import browser
 
+desktop = pytest.mark.parametrize("browser_window_size",
+                                  [(1920, 1080), (1600, 900)],
+                                  ids=["1920x1080", "1600x900"],
+                                  indirect=True
+                                  )
 
-resolution = pytest.mark.parametrize("browser_window_size",
-                                     [(1920, 1080), (1600, 900), (828, 1792), (750, 1334)],
-                                     ids=["1920x1080", "1600x900", "828x1792", "750x1334"],
-                                     indirect=True
-                                     )
+mobile = pytest.mark.parametrize("browser_window_size",
+                                 [(828, 1792), (750, 1334)],
+                                 ids=["828x1792", "750x1334"],
+                                 indirect=True
+                                 )
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(params=[(1920, 1080), (1600, 900), (828, 1792), (750, 1334)],
+                scope='function'
+                )
 def browser_window_size(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
@@ -18,18 +25,12 @@ def browser_window_size(request):
     browser.open('https://github.com/')
 
 
-@resolution
-def test_github_desktop():
-    try:
-        browser.element('a[href="/login"]').click()
-    except AssertionError:
-        pytest.xfail(reason="Mobile resolution")
+@desktop
+def test_github_desktop(browser_window_size):
+    browser.element('a[href="/login"]').click()
 
 
-@resolution
-def test_github_mobile():
-    try:
-        browser.element('svg.octicon-three-bars').click()
-        browser.element('a[href="/login"]').click()
-    except AssertionError:
-        pytest.xfail(reason="Desktop resolution")
+@mobile
+def test_github_mobile(browser_window_size):
+    browser.element('svg.octicon-three-bars').click()
+    browser.element('a[href="/login"]').click()
